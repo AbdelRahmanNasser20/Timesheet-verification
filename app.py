5861f1aaebaaafc6da97885b68e696cb3e9dda81
Abdel-Rahman Nasser <96590314+AbdelRahmanNasser20@users.noreply.github.com>
2024-06-13 16:22:10 -0700

    Turned on debug mode

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def home():
 # def hello():
 #     return 'Hello from Flask on Google Cloud Run!'
 
-if __name__ == '__main__':
+if __name__ == '__main__':    
     port = int(os.getenv('PORT', 8080))
     print("Hello running from port ", port)
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
